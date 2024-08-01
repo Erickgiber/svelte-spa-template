8d48a230f5258398fdaf32fd301c4b3a68411564
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 15:17:47 -0400

    fixed build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    })
+    adapter: adapter()
   }
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'build'
+  },
   resolve: {
     alias: {
       $lib: path.resolve(__dirname, 'src/lib'),
