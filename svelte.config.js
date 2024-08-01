f8664058c5150b27f0bf98081400d580e8144eef
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 15:53:40 -0400

    remove build directory config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.NODE_ENV === 'development'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
