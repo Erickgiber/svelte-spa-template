1a28528f2526f6630ac83cdcdcd45c8ab43fbab5
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 17:34:53 -0400

    fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 }
