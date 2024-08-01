511d01fc3f0415cf244d164a0199f800a2918c71
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 15:14:39 -0400

    Fixed htaccess and svelte config for build

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    })
+  }
 }
