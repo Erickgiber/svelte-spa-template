fdab7f2bba9f8e5a1a4ad38d7da7aba53686d36f
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 16:33:16 -0400

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/svelte-app-template/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
