e0c1c6f84f4fd2865e5acecc65b017aa36fe6c7b
Erick Ramirez <erickgiber7@gmail.com>
2024-08-01 15:39:03 -0400

    fixed svelte for build on github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: process.env.GH_PAGES_BUILD ? '/svelte-spa-template' : ''
+    }
   }
 };
