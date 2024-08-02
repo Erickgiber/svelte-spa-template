97dc41632d8c1dac583b29cd4dd95911c3a1ecc3
Erick Ramirez <erickgiber7@gmail.com>
2024-08-02 00:13:30 -0400

    formating

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import exp from 'constants'
   import { Link } from 'svelte-routing'
   import type { GetPropsParams } from 'types/props-anchor.types'
 
@@ -7,14 +6,18 @@
   export let replace: boolean = false
   export let className: string = ''
   let active: boolean = false
-  let activeClass = active ? 'link active' : 'link inactive'
 
   const props = (props: GetPropsParams): any => {
     active = props.isCurrent
-    activeClass = active ? 'link active' : 'link'
   }
 </script>
 
-<Link class={`${activeClass} ${className}`} {to} {replace} {...$$props} getProps={props}>
+<Link
+  class={`link ${active ? 'active' : ''} ${className}`}
+  {to}
+  {replace}
+  {...$$props}
+  getProps={props}
+>
   <slot />
 </Link>
